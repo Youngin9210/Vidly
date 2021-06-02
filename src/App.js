84929e068c6e6e514ff9f8de7788b96cd25886ca
Kyle <kyleyoung.9210@gmail.com>
2021-06-02 13:38:36 -0400

    rendering navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Customers from './components/customers';
 import Movies from './components/movies';
+import NavBar from './components/navbar';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
 
-function App() {
-	return (
-		<main className="container">
-			<Movies></Movies>
-		</main>
-	);
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<NavBar />
+				<main className="container">
+					<Switch>
+						<Route path="/movies" component={Movies}></Route>
+						<Route path="/customers" component={Customers}></Route>
+						<Route path="/rentals" component={Rentals}></Route>
+						<Route path="/not-found" component={NotFound}></Route>
+						<Redirect from="/" exact to="/movies" />
+						<Redirect to="/not-found" />
+					</Switch>
+				</main>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
